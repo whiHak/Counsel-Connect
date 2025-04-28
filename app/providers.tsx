b15b05517028cc5b6888fe03c8f6412882a05310
whiHak <betse1111111111@gmail.com>
2025-04-28 22:03:25 +0300

    fix: client error

--- app/providers.tsx
+++ app/providers.tsx
@@ -6,31 +6,31 @@ import { SessionProvider } from "next-auth/react";
 import { useToast } from "@/components/ui/use-toast";
 import { Toaster } from "@/components/ui/toaster";
 
-function ToastProvider({ children }: { children: React.ReactNode }) {
-  const searchParams = useSearchParams();
-  const { toast } = useToast();
-  const error = searchParams.get("error");
+// function ToastProvider({ children }: { children: React.ReactNode }) {
+//   const searchParams = useSearchParams();
+//   const { toast } = useToast();
+//   const error = searchParams.get("error");
 
-  useEffect(() => {
-    if (error) {
-      toast({
-        title: "Access Denied",
-        description: error,
-        variant: "destructive",
-      });
-    }
-  }, [error, toast]);
+//   useEffect(() => {
+//     if (error) {
+//       toast({
+//         title: "Access Denied",
+//         description: error,
+//         variant: "destructive",
+//       });
+//     }
+//   }, [error, toast]);
 
-  return children;
-}
+//   return children;
+// }
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <SessionProvider>
-      <ToastProvider>
+      {/* <ToastProvider> */}
         {children}
         <Toaster />
-      </ToastProvider>
+      {/* </ToastProvider> */}
     </SessionProvider>
   );
 } 
